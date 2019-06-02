cce13f868aaeec3ce0b2b00c2067f74cc66b54ed
vainamoinen <weixinluwx@foxmail.com>
2019-06-02 18:24:41 +0800

    query gen

--- io/IO.cpp
+++ io/IO.cpp
@@ -370,7 +370,7 @@ FILE* _sql_p, FILE* _res_p)
 
 int IO::rand_if_var(double ratio)
 {
-	int tmp_range=int(1/ratio);
+	int tmp_range=(int)(1/ratio);
 	printf("tmp_range: %d\n",tmp_range);
 	srand((unsigned)time(NULL)); 
 	int res=rand()%tmp_range;
